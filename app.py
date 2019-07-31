c9bef2d7500cde7a1dc5242e9f72b16f5936914f
myname21-meet <myname21@meet.mit.edu>
2019-07-31 04:31:21 -0400

    added things

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home_page():
+	food = ['pizza, mjadara, burger']
+	return render_template("index.html", no_food=False,
+	 food=food)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+	app.run(debug = True)
